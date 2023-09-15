053bb4ae22c8e3ff89e978ad374aa8cebd46e077
Nicolas Panozo <nicopanozoc@gmail.com>
2023-09-15 11:02:35 -0400

    initial UI added, backend almost finished to run API

---
+++ ui/script.js
@@ -0,0 +1,56 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMsg;
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi;
+}
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "http://127.0.0.1:8000/chat_data";
+    const messageElement = incomingChatLi.querySelector("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userContent: userMsg,
+        })
+    }
+
+    fetch(API_URL, requestOptions)
+        .then(res => res.json())
+        .then(data => {
+            messageElement.textContent = data.response; // Actualizamos la propiedad a la que se accede en la respuesta
+        })
+        .catch((error) => {
+            messageElement.textContent = "Oops! Something went wrong. Please try again.";
+        })
+        .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+const handleChat = () => {
+    userMsg = chatInput.value.trim();
+    if (!userMsg) return;
+    chatInput.value = "";
+
+    chatbox.appendChild(createChatLi(userMsg, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+sendChatBtn.addEventListener("click", handleChat);
